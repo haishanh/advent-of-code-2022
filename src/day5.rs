89036ba68fdc194ebb92b49360e9c851da9681b3
Haishan <haishanhan@gmail.com>
2022-12-05 22:08:49 +0800

    Add day5 test

--- src/day5.rs
+++ src/day5.rs
@@ -1,6 +1,13 @@
 use std::fs;
 use std::str;
 
+///     [D]    
+/// [N] [C]    
+/// [Z] [M] [P]
+///
+///  should become
+///
+///  [[b'Z', b'N'], [b'M', b'C', b'D'], [b'P']]
 fn parse_drawing(lines: &mut Vec<&str>) -> Vec<Vec<u8>> {
     let mut chart = Vec::new();
 
@@ -11,12 +18,14 @@ fn parse_drawing(lines: &mut Vec<&str>) -> Vec<Vec<u8>> {
         }
     }
 
+    // looping backward
     loop {
         let mut count = 0;
         if let Some(line) = lines.pop() {
             let bytes = line.as_bytes();
             for b in bytes {
                 if *b <= b'Z' && *b >= b'A' {
+                    // len of "[A] " is 4
                     chart[count / 4].push(*b);
                 }
                 count += 1;
@@ -28,6 +37,7 @@ fn parse_drawing(lines: &mut Vec<&str>) -> Vec<Vec<u8>> {
     chart
 }
 
+// parse "move 1 from 2 to 3" into vector [1, 2, 3]
 fn parse_instruction(input: &str) -> Vec<usize> {
     let bytes = input.as_bytes();
     let mut nums = Vec::new();
@@ -50,8 +60,9 @@ fn parse_instruction(input: &str) -> Vec<usize> {
     result
 }
 
-fn run_instruction(chart: &mut Vec<Vec<u8>>, instruction: &Vec<usize>) {
+fn run_instruction_part1(chart: &mut Vec<Vec<u8>>, instruction: &Vec<usize>) {
     let mut count = instruction[0];
+    // the pos in instruction is 1-based
     let from = instruction[1] - 1;
     let to = instruction[2] - 1;
 
@@ -71,13 +82,9 @@ fn run_instruction_part2(chart: &mut Vec<Vec<u8>>, instruction: &Vec<usize>) {
 
     let mut u: Vec<u8> = chart[from].drain(from_len - count..).collect();
     chart[to].append(&mut u);
-
-    // if let Some(b) = chart[from].pop() {
-    //     chart[to].push(b);
-    // }
 }
 
-pub fn part1() {
+fn process(run_instruction: fn(&mut Vec<Vec<u8>>, &Vec<usize>)) -> String {
     let content = fs::read_to_string("data/day5.txt").expect("expect file");
     let lines = content.lines();
 
@@ -99,8 +106,8 @@ pub fn part1() {
     for line in lines_iter {
         if line.starts_with("move") {
             let ins = parse_instruction(line);
-            // run_instruction(&mut chart, &ins);
-            run_instruction_part2(&mut chart, &ins);
+            run_instruction(&mut chart, &ins);
+            // run_instruction_part2(&mut chart, &ins);
         }
     }
 
@@ -109,17 +116,30 @@ pub fn part1() {
         let x = v.pop().unwrap();
         r.push(x);
     }
-    let x = str::from_utf8(&r).unwrap();
-    println!("result={}", x);
+    str::from_utf8(&r).unwrap().into()
 }
 
-pub fn part2() {
-    let content = fs::read_to_string("data/day5-sample.txt").expect("expect file");
-    let lines = content.lines();
+pub fn part1() -> String {
+    process(run_instruction_part1)
+}
 
-    let mut count = 0;
-    for line in lines {
-        //
+pub fn part2() -> String {
+    process(run_instruction_part2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = part1();
+        assert_eq!("JDTMRWCQJ", input);
     }
-    println!("count={}", count);
+
+    #[test]
+    fn test_part_two() {
+        let input = part2();
+        assert_eq!("VHJDDCWRD", input);
+    }
 }
