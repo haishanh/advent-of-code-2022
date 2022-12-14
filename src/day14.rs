4a69f3664ff0a156fb9bd9dffa2d372451a3efb6
Haishan <haishanhan@gmail.com>
2022-12-14 21:53:41 +0800

    Day14 optimization

--- src/day14.rs
+++ src/day14.rs
@@ -165,37 +165,48 @@ pub fn part2(filepath: &str) -> u32 {
 
     bound_y.1 += 2;
 
+    // a stack to record "holes"
+    // instead of start from (500, 0) every time, we can just pop() a point
+    // from this holes stack and start from there
+    // this 28X times faster compare with part2_v1 (without this technique)
+    // $ cargo bench day14
+    let mut holes = Vec::new();
+    holes.push((500, 0));
+
     let mut count = 0;
     loop {
-        let mut current = (500, 0);
+        let mut current = holes.pop().map_or((500, 0), |f| f);
         let state: SandState;
         loop {
-            let next = (current.0, current.1 + 1);
+            if obstacles.contains(&current) {
+                current = holes.pop().map_or((500, 0), |f| f);
+            }
 
-            if next.1 == bound_y.1 {
+            let mb = (current.0, current.1 + 1);
+            let lb = (current.0 - 1, current.1 + 1);
+            let rb = (current.0 + 1, current.1 + 1);
+
+            if mb.1 == bound_y.1 {
+                state = SandState::Rest;
+                obstacles.insert(current);
+                break;
+            } else if !obstacles.contains(&mb) {
+                holes.push(current);
+                current = mb;
+                continue;
+            } else if !obstacles.contains(&lb) {
+                holes.push(current);
+                current = lb;
+                continue;
+            } else if !obstacles.contains(&rb) {
+                holes.push(current);
+                current = rb;
+                continue;
+            } else {
                 state = SandState::Rest;
                 obstacles.insert(current);
                 break;
             }
-
-            if !obstacles.contains(&next) {
-                current = next;
-                continue;
-            }
-            let next = (current.0 - 1, current.1 + 1);
-            if !obstacles.contains(&next) {
-                current = next;
-                continue;
-            }
-            let next = (current.0 + 1, current.1 + 1);
-            if !obstacles.contains(&next) {
-                current = next;
-                continue;
-            }
-
-            state = SandState::Rest;
-            obstacles.insert(current);
-            break;
         }
 
         match state {
@@ -211,9 +222,65 @@ pub fn part2(filepath: &str) -> u32 {
     count
 }
 
+#[allow(dead_code)]
+pub fn part2_v1(filepath: &str) -> u32 {
+    let mut obstacles: HashSet<Point> = HashSet::new();
+    let (_, mut bound_y) = scan(filepath, &mut obstacles);
+
+    bound_y.1 += 2;
+
+    let mut count = 0;
+    loop {
+        let mut current = (500, 0);
+        let state: SandState;
+        loop {
+            if obstacles.contains(&current) {
+                current = (500, 0);
+            }
+
+            let mb = (current.0, current.1 + 1);
+            let lb = (current.0 - 1, current.1 + 1);
+            let rb = (current.0 + 1, current.1 + 1);
+
+            if mb.1 == bound_y.1 {
+                state = SandState::Rest;
+                obstacles.insert(current);
+                break;
+            } else if !obstacles.contains(&mb) {
+                current = mb;
+                continue;
+            } else if !obstacles.contains(&lb) {
+                current = lb;
+                continue;
+            } else if !obstacles.contains(&rb) {
+                current = rb;
+                continue;
+            } else {
+                state = SandState::Rest;
+                obstacles.insert(current);
+                break;
+            }
+        }
+
+        match state {
+            SandState::Rest => {
+                count += 1;
+            }
+            SandState::FallingForever => {}
+        }
+        if obstacles.contains(&(500, 0)) {
+            break;
+        }
+    }
+    count
+}
+
+extern crate test;
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test::Bencher;
 
     #[test]
     fn test_part1() {
@@ -225,5 +292,21 @@ mod tests {
     fn test_part2() {
         assert_eq!(93, part2("data/day14-sample.txt"));
         assert_eq!(26729, part2("data/day14.txt"));
+    }
+
+    #[test]
+    fn test_part2_v1() {
+        assert_eq!(93, part2_v1("data/day14-sample.txt"));
+        assert_eq!(26729, part2_v1("data/day14.txt"));
+    }
+
+    #[bench]
+    fn bench_part2(b: &mut Bencher) {
+        b.iter(|| part2("data/day14.txt"));
+    }
+
+    #[bench]
+    fn bench_part2_v1(b: &mut Bencher) {
+        b.iter(|| part2_v1("data/day14.txt"));
     }
 }
